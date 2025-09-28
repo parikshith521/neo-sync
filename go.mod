ce412516550c00fd96ae19688f28e2b34eb26fed
parikshith521 <parikshithz521@gmail.com>
2025-09-28 16:03:25 +0530

    implemented recursive directory watching

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/parikshith521/neo-sync
+
+go 1.24.6
+
+require (
+	github.com/fsnotify/fsnotify v1.9.0 // indirect
+	golang.org/x/sys v0.13.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"log"
+	"os"
+	"path/filepath"
+
+	"github.com/fsnotify/fsnotify"
+)
+
+func main() {
+	// Create new watcher.
+	watcher, err := fsnotify.NewWatcher()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer watcher.Close()
+
+	// Start listening for events.
+	go func() {
+		// Infinite loop to listen.
+		for {
+			select {
+			case event, ok := <-watcher.Events:
+				if !ok {
+					return
+				}
+				log.Printf("EVENT: %s | OPERATION: %s", event.Name, event.Op)
+				// Listen to directory creation events.
+				if event.Has(fsnotify.Create) {
+					info, err := os.Stat(event.Name)
+					if err == nil && info.IsDir() {
+						err := watcher.Add(event.Name)
+						if err != nil {
+							log.Printf("Error adding new directory %s to watcher: %v", event.Name, err)
+						} else {
+							log.Printf("Added new directory to watcher: %s", event.Name)
+						}
+					}
+				}
+			case err, ok := <-watcher.Errors:
+				if !ok {
+					return
+				}
+				log.Println("Wacher Error: ", err)
+			}
+		}
+	}()
+
+	// Add tmp directory to be watched (recursive).
+	// Should be passed a CLA in the future.
+	targetDir := "/home/pari/Desktop/Dev/tmp"
+	err = filepath.WalkDir(targetDir, func(path string, d os.DirEntry, err error) error {
+		if d.IsDir() {
+			err := watcher.Add(path)
+			if err != nil {
+				return err
+			}
+			log.Println("Watching directory: ", path)
+		}
+		return nil
+	})
+	if err != nil {
+		log.Fatalf("Error walking target directory: %v", err)
+	}
+
+	// Block main gorountine forever.
+	<-make(chan struct{})
+}
