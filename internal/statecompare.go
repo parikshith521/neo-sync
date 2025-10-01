2f18ff5c03128580ab360975a3d3c1dd1f730b4b
parikshith521 <parikshithz521@gmail.com>
2025-10-01 12:19:28 +0530

    state comparison update

--- internal/statecompare.go
+++ internal/statecompare.go
@@ -1,18 +1,53 @@
 package internal
 
-import "github.com/parikshith521/neo-sync/models"
+import (
+	"fmt"
+	"path/filepath"
 
-func Compare(localDirState models.DirState, localFileState models.FileState, remoteDirState models.DirState, remoteFileState models.FileState) []string {
+	"github.com/parikshith521/neo-sync/models"
+)
+
+func getRootDir(dirState models.DirState) string {
+	var rootDir string
+	for path := range dirState {
+		if rootDir == "" || len(path) < len(rootDir) {
+			rootDir = path
+		}
+	}
+	return rootDir
+}
+
+func convertRemoteToLocalPath(localRoot, remoteRoot, remotePath string) (string, error) {
+	relativePath, err := filepath.Rel(remoteRoot, remotePath)
+	if err != nil {
+		return "", fmt.Errorf("the provided remote path is not inside the remote root: %w", err)
+	}
+	localPath := filepath.Join(localRoot, relativePath)
+	return localPath, nil
+}
+
+func Compare(localDirState models.DirState, localFileState models.FileState, remoteDirState models.DirState, remoteFileState models.FileState) ([]string, error) {
 	var actions []string
+
+	localRoot := getRootDir(localDirState)
+	remoteRoot := getRootDir(remoteDirState)
 	// Directory state comparison
 	for dirPath := range remoteDirState {
-		if _, ok := localDirState[dirPath]; !ok {
-			command := "mkdir " + dirPath
+		localDirPath, err := convertRemoteToLocalPath(localRoot, remoteRoot, dirPath)
+		if err != nil {
+			return nil, err
+		}
+		if _, ok := localDirState[localDirPath]; !ok {
+			command := "mkdir " + localDirPath
 			actions = append(actions, command)
 		}
 	}
 	for dirPath := range localDirState {
-		if _, ok := remoteDirState[dirPath]; !ok {
+		remoteDirPath, err := convertRemoteToLocalPath(remoteRoot, localRoot, dirPath)
+		if err != nil {
+			return nil, err
+		}
+		if _, ok := remoteDirState[remoteDirPath]; !ok {
 			command := "rm -r " + dirPath
 			actions = append(actions, command)
 		}
@@ -20,17 +55,25 @@ func Compare(localDirState models.DirState, localFileState models.FileState, rem
 
 	// File state comparison
 	for filePath := range remoteFileState {
-		if _, ok := localFileState[filePath]; !ok {
-			command := "touch " + filePath
+		localFilePath, err := convertRemoteToLocalPath(localRoot, remoteRoot, filePath)
+		if err != nil {
+			return nil, err
+		}
+		if _, ok := localFileState[localFilePath]; !ok {
+			command := "touch " + localFilePath
 			actions = append(actions, command)
 		}
 	}
 	for filePath := range localFileState {
-		if _, ok := remoteFileState[filePath]; !ok {
+		remoteFilePath, err := convertRemoteToLocalPath(remoteRoot, localRoot, filePath)
+		if err != nil {
+			return nil, err
+		}
+		if _, ok := remoteFileState[remoteFilePath]; !ok {
 			command := "rm " + filePath
 			actions = append(actions, command)
 		}
 	}
 
-	return actions
+	return actions, nil
 }
