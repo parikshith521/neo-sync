34cb97170cb5adf776f266b8839dd4a203ad40d4
parikshith521 <parikshithz521@gmail.com>
2025-10-06 17:44:38 +0530

    pretty update

--- internal/action.go
+++ internal/action.go
@@ -7,7 +7,7 @@ import (
 
 func ExecuteCommands(commands []string) error {
 	for _, cmdStr := range commands {
-		fmt.Printf("▶️ Executing: %s\n", cmdStr)
+		fmt.Printf("Executing: %s\n", cmdStr)
 		cmd := exec.Command("sh", "-c", cmdStr)
 		if err := cmd.Run(); err != nil {
 			return fmt.Errorf("failed to execute command '%s': %w", cmdStr, err)
